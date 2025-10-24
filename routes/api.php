5cc8b965afadea293fc61409df7dbaf7a9a387e9
Ksarten <kukharenok96@mail.ru>
2025-10-24 13:55:56 +0500

    Установка Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,29 @@
 <?php
 
-
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FilmControllerApi;
 use App\Http\Controllers\HallControllerApi;
 use App\Http\Controllers\SeatControllerApi;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/seat', [SeatControllerApi::class, 'index']);
-Route::get('/seat/{id}', [SeatControllerApi::class, 'show']);
+
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/hall', [HallControllerApi::class, 'index']);
 Route::get('/hall/{id}', [HallControllerApi::class, 'show']);
 
 Route::get('/film', [FilmControllerApi::class, 'index']);
 Route::get('/film/{id}', [FilmControllerApi::class, 'show']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::get('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/seat', [SeatControllerApi::class, 'index']);
+    Route::get('/seat/{id}', [SeatControllerApi::class, 'show']);
+});
